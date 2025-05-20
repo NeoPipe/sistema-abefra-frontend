2de43793575ed4f504256fa36137a153a39d36d1
Maicon Giehl <maicongiehl@gmail.com>
2025-05-20 20:18:29 -0300

    feat: signin

--- src/pages/Login/Login.component.tsx
+++ src/pages/Login/Login.component.tsx
@@ -1,13 +1,37 @@
+import { useFormik } from "formik";
+import { signin } from "../../api/auth";
+import Button from "../../components/atoms/Button";
 import Container from "../../components/atoms/Container";
 import Input from "../../components/atoms/Input";
 import Typography from "../../components/atoms/Typography";
 
 const LoginComponent = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      signin({ username: values.username, password: values.password });
+    },
+  });
+
   return (
     <Container fullCentralized fullHeight>
       <Typography as="h1">Sistema ABEFRA</Typography>
-      <Input placeholder="Usuário"></Input>
-      <Input placeholder="senha"></Input>
+      <form action="" onSubmit={formik.handleSubmit}>
+        <Input
+          placeholder="Usuário"
+          id="username"
+          onChange={formik.handleChange}
+        ></Input>
+        <Input
+          placeholder="senha"
+          id="password"
+          onChange={formik.handleChange}
+        ></Input>
+        <Button type="submit">asdasdsa</Button>
+      </form>
     </Container>
   );
 };
