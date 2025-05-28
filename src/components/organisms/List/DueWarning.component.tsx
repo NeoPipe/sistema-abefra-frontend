f42fa9693ec0ccf344601d3449c540728cd2a885
cauavorpagel <cauamvorpagel@gmail.com>
2025-05-28 19:40:45 -0300

    feat: list-component

---
+++ src/components/organisms/List/DueWarning.component.tsx
@@ -0,0 +1,14 @@
+import type DueWarningComponent from ".";
+import Button from "../..//atoms/Button";
+
+const button = Button;
+
+function selectBTN(Button) {
+  <Button as="h2"></Button>;
+
+  <Button></Button>;
+
+  <Button></Button>;
+}
+
+export default DueWarningComponent;
